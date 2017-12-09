ff64a3bc1defcd370ed93807e4a81cb62144740c
Nitai <benshaul.n@gmail.com>
2017-12-09 21:50:10 +0200

    Convert orders to data tables

--- src/app/admin/components/admin-orders/admin-orders.component.ts
+++ src/app/admin/components/admin-orders/admin-orders.component.ts
@@ -1,25 +1,51 @@
 import { FirebaseListObservable } from 'angularfire2/database/firebase_list_observable';
 import { UserService } from '../../../shared/services/user.service';
-import { Order } from '../../../shared/models/order';
+import { Order, OrderView } from '../../../shared/models/order';
 import { OrderService } from '../../../shared/services/order.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { DataTableResource } from 'angular-4-data-table';
 
 @Component({
   selector: 'app-admin-orders',
   templateUrl: './admin-orders.component.html',
   styleUrls: ['./admin-orders.component.css']
 })
-export class AdminOrdersComponent {
-  orders$;
-  userName: string;
+export class AdminOrdersComponent implements OnInit, OnDestroy {
+  orders: Order[];
+  subscription: Subscription;
+  tableResource: DataTableResource<Order>;
+  items: Order[] = [];
+  itemCount: number; 
+  userName;
 
   constructor(
     private orderService: OrderService,
     private userService: UserService
   ) { 
-    this.orders$ = orderService.getOrders();    
+    this.subscription = this.orderService.getOrders()
+    .subscribe(orders => {
+      this.orders = orders;
+      this.initializeTable(orders);
+    });
   }
   
+  private initializeTable(orders: Order[]) {
+    this.tableResource = new DataTableResource(orders);
+    this.tableResource.query({ offset: 0 })
+      .then(items => this.items = items);
+    this.tableResource.count()
+      .then(count => this.itemCount = count);
+  }
+
+  reloadItems(params) {
+    if (!this.tableResource) return;
+
+    this.tableResource.query(params)
+      .then(items => this.items = items);    
+  }
+
+
  getUserNameByUserId(userId) {
   this.userService.getUserByUserId(userId).forEach(u => {
     this.userName = u.userName;
@@ -31,4 +57,10 @@ export class AdminOrdersComponent {
     this.orderService.markShipped(orderId, new Date().getTime()); //When click on Mark as Shipped, it send the current date time.
   }
   
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  ngOnInit() {
+  }
 }
--- src/app/shopping/components/my-orders/my-orders.component.ts
+++ src/app/shopping/components/my-orders/my-orders.component.ts
@@ -2,6 +2,10 @@ import { AuthService } from 'shared/services/auth.service';
 import { OrderService } from '../../../shared/services/order.service';
 import { Component, OnInit } from '@angular/core';
 import 'rxjs/add/operator/switchMap';
+import { Order } from 'shared/models/order';
+import { Subscription } from 'rxjs/Subscription';
+import { DataTableResource } from 'angular-4-data-table';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-my-orders',
@@ -9,12 +13,41 @@ import 'rxjs/add/operator/switchMap';
   styleUrls: ['./my-orders.component.css']
 })
 export class MyOrdersComponent {
-  orders$;
+  orders: Observable<any>[];
+  subscription: Subscription;
+  tableResource: DataTableResource<Order>;
+  items: Order[] = [];
+  itemCount: number; 
+
   
   constructor(
     private authService: AuthService,
-    private orderService: OrderService) { 
-
-    this.orders$ = authService.user$.switchMap(u => orderService.getOrdersByUser(u.uid));
+    private orderService: OrderService) {
+       authService.user$.take(1).subscribe(u => {
+         // possible memory leak need to check if unsubscribe is needed after using take(1)
+        this.subscription = this.orderService.getOrdersByUser(u.uid)
+        .subscribe(orders => {
+          this.orders = orders;
+          this.initializeTable(orders);
+        });
+       }); 
   }
+
+
+  private initializeTable(orders: Order[]) {
+    this.tableResource = new DataTableResource(orders);
+    this.tableResource.query({ offset: 0 })
+      .then(items => this.items = items);
+    this.tableResource.count()
+      .then(count => this.itemCount = count);
+  }
+
+  reloadItems(params) {
+    if (!this.tableResource) return;
+
+    this.tableResource.query(params)
+      .then(items => this.items = items);    
+  }
+
+
 }
