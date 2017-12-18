9dab49225dc6c318f5b26ebffa54dc5ea48b95a3
Nitai <benshaul.n@gmail.com>
2017-12-18 14:50:51 +0200

    Change all pages to show title as card, added confirmation for handeling messages. added more validation for contact-us form and a form reset + restyling to some navbar elements

---
+++ src/app/shopping/components/contact-success/contact-success.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import {  MatDialogRef, MAT_DIALOG_DATA } from '@angular/material'
+
+@Component({
+  selector: 'app-contact-success',
+  templateUrl: './contact-success.component.html',
+  styleUrls: ['./contact-success.component.css']
+})
+export class ContactSuccessComponent implements OnInit {
+
+  constructor(private mdr: MatDialogRef<ContactSuccessComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+    
+    onNoClick(): void {
+      this.mdr.close();
+    }
+
+    close() {
+      this.mdr.close();      
+    }
+  ngOnInit() {
+  }
+
+}
